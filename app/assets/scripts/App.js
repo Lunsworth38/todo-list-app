cce5cfadd3a15fa04918c57cf6ce7022526c5408
Lewis <lunsworth@live.com>
2018-07-17 19:25:11 +0100

    add functionality to display todos in ul

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,47 +1,27 @@
 var todoList = {
   todos: [],
   
-  displayTodos: function() {
-    if (this.todos.length === 0) {
-	    console.log("Your todo list is empty!");
-	 } else {
-	    console.log("My todos:");
-	    for(var i=0; i< this.todos.length; i++) {
-	      if(this.todos[i].completed === true) {
-	         var check ="(x)";
-	      } else {
-	         var check ="( )";
-	      };
-	     console.log(check, this.todos[i].todoText);
-	    };
-	  };
-  },
-  
 	//add todo
 	addTodo: function(todoText) {
 		this.todos.push({
 		  todoText: todoText,
 		  completed: false
 		});
-		this.displayTodos(); 
 	},
 	
   //delete Todo
 	deleteTodo: function(position) {
 		this.todos.splice(position,1);
-		this.displayTodos();
 	},
 	
   //change a Todo
 	changeTodo: function(position, todoText) {
 		this.todos[position].todoText = todoText;
-		this.displayTodos();
 	},
 	
 	toggleCompleted: function(position) {
 	  var todo = this.todos[position];
 	  todo.completed = !todo.completed;
-	  this.displayTodos();
 	},
 
   toggleAll: function() {
@@ -62,19 +42,16 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-    this.displayTodos();
   }
 
 };
 
 var handlers = {
-  displayTodos: function() {
-    todoList.displayTodos();
-  },
   addTodo: function() {
     var addTodoTextInput = document.getElementById("add-todo-text-input");
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value="";
+    view.displayTodos();
   },
   changeTodo: function() {
     var changeTodoPositionInput = document.getElementById("change-todo-position-input");
@@ -82,21 +59,45 @@ var handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     changeTodoPositionInput.value = "";
     changeTodoTextInput.value = "";
+    view.displayTodos();
   },
   deleteTodo: function() {
     var deleteTodoPositionInput = document.getElementById("delete-todo-position-input");
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = "";
+    view.displayTodos();
   },
   toggleCompleted: function() {
     var toggleCompletedPositionInput = document.getElementById("toggle-completed-position-input");
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = "";
+    view.displayTodos();
   },
   toggleAll:  function() {
-  todoList.toggleAll();
+  	todoList.toggleAll();
+  	view.displayTodos();
   }
 };
 
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector("ul");
+    todosUl.innerHTML = "";
+    for(var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement("li");
+      var todo = todoList.todos[i];
+      var todoTextWithCompletion = "";
+      
+      if (todo.completed === true) {
+        todoTextWithCompletion = "(x) " + todo.todoText;
+      } else {
+        todoTextWithCompletion = "( ) " + todo.todoText;
+      }
+      
+      todoLi.textContent = todoTextWithCompletion;
+      todosUl.appendChild(todoLi);
+    }
+  }
+};
 
 
